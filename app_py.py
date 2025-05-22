94d50de0dd2bc2d0e8c57ca3f8ca9508e9b2ed91
Jhae1213 <joesafariolan2004@gmail.com>
2025-05-22 09:41:13 -0700

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1EmBzPH70-wbCZA5hN1OLgncl3bc_CJZw
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
